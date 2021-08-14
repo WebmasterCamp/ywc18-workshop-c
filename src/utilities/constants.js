a40055213a4b4ce7fb3bcc1dd12c3144c24b6556
UnkAtreus <unk.atreus@gmail.com>
2021-08-14 09:51:52 +0700

    mock constant

---
+++ src/utilities/constants.js
@@ -0,0 +1,17 @@
+export const BloodType = {
+  A: "A",
+  B: "B",
+  O: "O",
+  AB: "AB",
+  Aneg: "A-",
+  Bneg: "B-",
+  Oneg: "O-",
+  ABneg: "AB-",
+};
+
+export const user = {
+  username: "YWC 18",
+  point: 10,
+  history: [],
+  bloodType: BloodType.A,
+};
