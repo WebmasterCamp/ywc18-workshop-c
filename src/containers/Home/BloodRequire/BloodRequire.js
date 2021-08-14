7abd6ae78e17ecb81baa85a2d2c375eb728a1165
Thanapat Teerarattanyu <37036113+TJTHANAPAT@users.noreply.github.com>
2021-08-14 12:04:02 +0700

    BloodRequire

--- src/containers/Home/BloodRequire/BloodRequire.js
+++ src/containers/Home/BloodRequire/BloodRequire.js
@@ -1,4 +1,9 @@
-import { Row, Col, Space, Typography, Divider } from 'antd';
+import { Row, Col, Space, Typography, Flex } from 'antd';
+import { default as BloodTypeA } from '../../../assets/svg/blood-type/BloodTypeA.svg';
+import { default as BloodTypeB } from '../../../assets/svg/blood-type/BloodTypeB.svg';
+import { default as BloodTypeO } from '../../../assets/svg/blood-type/BloodTypeO.svg';
+import { default as BloodTypeAB } from '../../../assets/svg/blood-type/BloodTypeAB.svg';
+
 const { Title, Text } = Typography;
 
 const bloodRequireData = [
@@ -7,34 +12,42 @@ const bloodRequireData = [
     requiredUnitPerMonth: 12200,
     aqquiredUnit: 2612,
     requiredUnit: 9588,
+    logo: BloodTypeA,
   },
   {
     type: 'B',
     requiredUnitPerMonth: 18300,
     aqquiredUnit: 4433,
     requiredUnit: 13867,
+    logo: BloodTypeB,
   },
   {
     type: 'O',
     requiredUnitPerMonth: 24400,
     aqquiredUnit: 5147,
     requiredUnit: 19253,
+    logo: BloodTypeO,
   },
   {
     type: 'AB',
     requiredUnitPerMonth: 6100,
     aqquiredUnit: 1132,
     requiredUnit: 4968,
+    logo: BloodTypeAB,
   },
 ];
-
-const style = { background: '#0092ff', padding: '8px 0' };
 
 const BloodRequire = () => {
   return (
     <div className="max-w-screen-xl m-auto bg-white rounded-xl shadow-xl p-5 ">
-      <Title level={2}>ปริมาณเลือดที่ขาดรายวัน</Title>
-      <Text>ข้อมูล ณ วันที่ 15 สิงหาคม 2564</Text>
+      <Row>
+        <Col span={8}>
+          <Title level={3}>ปริมาณเลือดที่ขาดรายวัน</Title>
+        </Col>
+        <Col span={8} offset={8}>
+          <Text className="float-right">ข้อมูล ณ วันที่ 15 สิงหาคม 2564</Text>
+        </Col>
+      </Row>
 
       <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
         {bloodRequireData.map(bloodRequire => {
@@ -49,16 +62,14 @@ const BloodRequire = () => {
 
 const RequireCard = props => {
   const { bloodRequire } = props;
-  const { type, requiredUnitPerMonth } = bloodRequire;
+  const { requiredUnitPerMonth, logo } = bloodRequire;
   return (
     <Col className="gutter-row" span={6}>
       <Space>
-        <Title>{type}</Title>
-        <Space direction="vertical">
+        <img src={logo} />
+        <Space direction="vertical" className="ml-5">
           <Text>ความต้องการโลหิต</Text>
-          <Title level={4} style={{ margin: 0 }}>
-            {requiredUnitPerMonth} ยูนิต/เดือน
-          </Title>
+          <Title level={4}>{requiredUnitPerMonth} ยูนิต/เดือน</Title>
         </Space>
       </Space>
     </Col>
