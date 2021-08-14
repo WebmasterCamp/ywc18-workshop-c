b86c99c11e2a6384f33c0b596e560988e7541a54
UnkAtreus <unk.atreus@gmail.com>
2021-08-14 15:09:49 +0700

    add Banner

---
+++ src/containers/Home/RohitBanner/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Banner from "../../../assets/svg/banner.svg";
+
+const RohitBanner = () => {
+  return (
+    <div className="max-w-screen-xl m-auto mb-12">
+      <img
+        src={Banner}
+        alt="banner"
+        className="rounded-2xl"
+        style={{ width: "-webkit-fill-available" }}
+      />
+    </div>
+  );
+};
+
+export default RohitBanner;
