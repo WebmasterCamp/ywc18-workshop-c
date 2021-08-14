1b39707d9aca09d8afaa72b36082ae932d0fc4a4
Thanapat Teerarattanyu <37036113+TJTHANAPAT@users.noreply.github.com>
2021-08-14 17:15:38 +0700

    Redeem

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Home from './containers/Home';
 import Blog from './containers/Blog';
 import DonateLocation from './containers/DonateLocation';
 import Redeem from './containers/Redeem';
-import RewardDetail from './containers/RewardDetail';
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
         <Route exact path="/donate" component={DonateLocation} />
         <Route exact path="/blog" component={Blog} />
         <Route exact path="/redeem" component={Redeem} />
-        <Route exact path="/reward" component={RewardDetail} />
         <Route exact path="/" component={Home} />
       </Switch>
     </Router>
