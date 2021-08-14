fb7d4f954bc172ba5164c823bfb8c832d3eb03f7
UnkAtreus <unk.atreus@gmail.com>
2021-08-14 13:45:19 +0700

    merge

--- src/containers/DonateLocation/index.js
+++ src/containers/DonateLocation/index.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { Layout, Col, Row } from "antd";
+import BloodDonation from "../Home/BloodDonation";
 
 const DonateLocation = () => {
-  return <div>test</div>;
+  const { Header, Footer, Content } = Layout;
+
+  return (
+    <Layout>
+      <Header>is Header</Header>
+      <Content>
+        <div className="max-w-screen-lg m-auto mt-8">
+          <BloodDonation />
+
+          <div className="rounded-2xl shadow-md h-auto bg-white  p-8">
+            <h1 className="text-2xl">เขตปธุมวัน</h1>
+            <Row>
+              <Col flex={3}>2 / 5</Col>
+              <Col flex={2}>3 / 5</Col>
+              <Col flex={2}>3 / 5</Col>
+            </Row>
+          </div>
+        </div>
+      </Content>
+      <Footer>Footer</Footer>
+    </Layout>
+  );
 };
 
 export default DonateLocation;
